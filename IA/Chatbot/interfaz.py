aca9ff424c0b49f0dea8d2ca5957cf1e0608f98f
Salatiel <jesussalatiel21@gmail.com>
2018-11-11 23:52:02 -0600

    Mod 1

--- IA/Chatbot/interfaz.py
+++ IA/Chatbot/interfaz.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import messagebox  
+import tkinter as tk
 import os 
+
+from tkinter.filedialog import *
 
 class controlPanel():
     #http://pharalax.com/blog/python-desarrollo-de-interfaces-graficas-con-tkinter-labelsbuttonsentrys/
@@ -14,40 +17,55 @@ class controlPanel():
         self.photo.grid(column=0, row=0, padx=(50,50), pady=(10,10))
         self.photo.columnconfigure(0, weight=1)
         self.photo.rowconfigure(0, weight=1) 
-        ###########################################
-        #Configuracion para la toma de fotos
-        #self.label_0 = Label(self.app, text="Tomar foto", width=20, font=("bold", 12))
-        #self.label_0.grid(column=2, row=1, sticky=(W,E))
+        ############
+        
+        ################################################################
 
-        #entry_1 = Entry(app)
-        #entry_1.grid(column=1, row=1)
-        """ Configuracion del boton para tomar foto"""
-        """, bg='brown', fg='white' """
-        btn_photo = Button(self.app, text='Tomar Foto', width=12, command=self.takePicture)
-        btn_photo.grid(column=1, row=1)
-        """ Configuracion del boton para tomar audio"""
-        btn_audio = Button(self.app, text='Hablar',width=12, command=self.speechAudio)
-        btn_audio.grid(column=1, row=2)
-        """ Escribir notas"""
-        btn_notes = Button(self.app, text='Crear notas',width=12, command=self.showNotes)
-        btn_notes.grid(column=4, row=1)
+        """
+        #Creacion de botones en la lista principal 
+        btnCreate('Nombre del boton', 'cordenada x', 'cordenada y', 'evento')
+        """        
+        self.btnCreate(self.app,'Crear notas', 1, 1, self.createNotes)
+        self.btnCreate(self.app,'Audio', 1, 2, self.speechAudio)
+        self.btnCreate(self.app,'Buscar Foto', 1, 3, self.loadFile)
+        
+        return self.app.mainloop()
 
+    def btnCreate(self,windows,text,column,row,execMethod):
+        """Plantilla para crear botones"""
+        btn_notes = Button(windows, text=text,width=12, command=execMethod)
+        btn_notes.grid(column=column, row=row)
 
-        #Iniciar interfaz
-        self.app.mainloop()
-    def showNotes(self):
-        print('Notas')
+    def createNotes(self):
+        notes = Tk()
+        notes.geometry("500x400")
+        notes.title('Crear Nota')
+
+        entry = Text(notes)
+        entry.place(x=20, y=20, height=300, width=460) 
+
+        btn_save_note = Button(notes, text="Guardar", command=lambda: self.getText((entry.get('1.0','end-1c'))) )
+        btn_save_note.place(x=200,y=350,height=30, width=100)
+       
+
+    def getText(self, text):
+        print(text+ ' '+ str(type(text)))
+
     def speechAudio(self):
         print('Audio')
+        
     def takePicture(self):
-        print('Tomar Foto')
+        print('Tomar foto')
+
+    def loadFile(self):
+        path= askopenfilename(filetypes=( ("Imagenes", "*.jpg; *.jpeg"), ("All files", "*.*") ))
+        print('Path: '+path +' ' + str(type(path)))
 
     def execPy(self, archivo):
         os.system('python '+archivo)
 
 def main():
-        app = controlPanel()
-        return 0
+        return controlPanel()
 
 if __name__ == '__main__':
     main()
